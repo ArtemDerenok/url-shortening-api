2e6db4e792943628b66cad213e3833a3af539970
ArtemDerenok <derenokartem95@gmail.com>
2023-07-24 21:17:32 +0300

    feat: add boost component

--- src/components/boost/Boost.tsx
+++ src/components/boost/Boost.tsx
@@ -1,7 +1,17 @@
+import Button from "../button/Button";
+import bgImg from "../../assets/images/bg-boost-desktop.svg";
 import styles from "./Boost.module.scss";
 
 const Boost = () => {
-  return <section className={styles.boost}></section>;
+  return (
+    <section className={styles.boost}>
+      <div className={styles.boost__container}>
+        <h2 className={styles.boost__title}>Boost your links today</h2>
+        <Button text="Get Started" callback={() => {}} className="btn-about" />
+      </div>
+      <img className={styles.boost__img} src={bgImg} alt="background" />
+    </section>
+  );
 };
 
 export default Boost;
--- src/components/main-page/MainPage.tsx
+++ src/components/main-page/MainPage.tsx
@@ -1,4 +1,5 @@
 import About from "../about/About";
+import Boost from "../boost/Boost";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
 import styles from "./MainPage.module.scss";
@@ -8,6 +9,7 @@ const MainPage = () => {
     <div className={styles.page}>
       <Header />
       <About />
+      <Boost />
       <Footer />
     </div>
   );
